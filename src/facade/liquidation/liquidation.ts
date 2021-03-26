36e69d5994d4646da0cff194013160a3ba050b7c
jess <jesse@terra.money>
2021-03-26 23:55:42 +0900

    chore: unused

--- src/facade/liquidation/liquidation.ts
+++
@@ -1,9 +0,0 @@
-import { AddressProvider } from '../../address-provider';
-
-export class Liquidation {
-  addressProvider!: AddressProvider;
-
-  constructor(addressProvider: AddressProvider) {
-    this.addressProvider = addressProvider;
-  }
-}
