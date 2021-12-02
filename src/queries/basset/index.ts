76cdae874323c515ee14534ec5bc466f8d8dee05
Cain O'Sullivan <cain.osullivan@gmail.com>
2021-12-02 12:40:59 +0800

    Feat/remove bluna validator (#45)
    * chore: updated version number
    
    * chore:removed the bLuna validator related items

--- src/queries/basset/index.ts
+++ src/queries/basset/index.ts
@@ -1,7 +1,6 @@
 export * from './hub-config';
 export * from './hub-params';
 export * from './hub-state';
-export * from './hub-whitelisted-validators';
 export * from './hub-current-batch';
 export * from './hub-withdrawable';
 export * from './hub-all-history';
