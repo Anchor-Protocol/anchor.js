e1226a644824aba1438e72ffea7ea70047947268
Sergey Ratiashvili <sergey.ratiashvili@aggregion.com>
2021-12-09 16:49:02 +0300

    feat: update interfaces for new version of contracts

--- src/queries/basset/hub-state.ts
+++ src/queries/basset/hub-state.ts
@@ -7,8 +7,12 @@ interface Option {
 }
 
 interface StateResponse {
-  exchange_rate: string;
-  total_bond_amount: string;
+  exchange_rate: string; //deprecated
+  bluna_exchange_rate: string;
+  stluna_exchange_rate: string;
+  total_bond_amount: string; //deprecated
+  total_bond_bluna_amount: string;
+  total_bond_stluna_amount: string;
   last_index_modification: number;
   prev_hub_balance: string;
   actual_unbonded_amount: string;
